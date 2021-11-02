56f8d92898ee54b598241d572055de54aff4fda5
ahtchow <ahtchow@gmail.com>
2021-11-02 00:50:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
+from pynput.keyboard import Key, Controller
 import json, os
+
+running = True
+keyboard = Controller()
+pygame.init()
 
 #Initialize controller
 joysticks = []
@@ -11,3 +16,28 @@ for joystick in joysticks:
 # Read in key mappings
 with open(os.path.join("ps4_keys_main.json"), 'r+') as file:
     button_keys = json.load(file)
+
+
+while running:
+
+    for event in pygame.event.get():
+        # Press Down On A Button
+        if event.type == pygame.JOYBUTTONDOWN:
+            if event.button == button_keys['left_arrow']:
+                keyboard.press('a')
+                keyboard.release('a')
+            if event.button == button_keys['right_arrow']:
+                keyboard.press('d')
+                keyboard.release('d')
+            if event.button == button_keys['down_arrow']:
+                keyboard.press('c')
+                keyboard.release('c')
+            if event.button == button_keys['up_arrow']:
+                keyboard.press('b')
+                keyboard.release('b')
+            if event.button == button_keys['PS']:
+                running = False
+                print("FAIL SAFE")
+        # Press Down On A Button
+        if event.type == pygame.JOYBUTTONUP:
+            pass
